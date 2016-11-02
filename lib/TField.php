64c1f14d3ff8b96920f89f4367777588d4e289a5
Виталий Акмашев <va@crtweb.ru>
2016-11-02 15:39:50 +0500

    add field trait and fix it's logic

---
+++ lib/TField.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace bxar;
+
+/**
+ * Трэйт, который реализует базовые функции IField
+ */
+interface TField
+{
+    /**
+     * @var mixed
+     */
+    protected $_fieldValueFromModel = null;
+
+    /**
+     * Задает значение, которое будет обработано для вывода
+     *
+     * @param mixed $value
+     *
+     * @return \bxar\IField
+     */
+    public function setFieldValueFromModel($value)
+    {
+        $this->_fieldValueFromModel = $value;
+    }
+
+    /**
+     * Возвращает значение, обработанное для вывода
+     *
+     * @return mixed
+     */
+    public function getFieldValueFromModel()
+    {
+        return $this->_fieldValueFromModel;
+    }
+
+    /**
+     * Возвращает значение, обработанное для записи в репозиторий
+     *
+     * @return mixed
+     */
+    public function getFieldValueFromModelToRepo()
+    {
+        return $this->_fieldValueFromModel;
+    }
+
+    /**
+     * Сбрасывает все настройки данного поля
+     *
+     * @return \bxar\IField
+     */
+    public function clearField()
+    {
+        $this->_fieldValueFromModel = null;
+    }
+}
