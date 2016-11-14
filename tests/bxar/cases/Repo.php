65492621c8a6fd1111f6a30ded90f3d967726191
Your Name <you@example.com>
2016-11-15 00:37:32 +0500

    fix

---
+++ tests/bxar/cases/Repo.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace marvin255\bxar\tests\cases;
+
+abstract class Repo extends \PHPUnit_Framework_TestCase
+{
+    abstract public function getObject();
+}
---
+++ tests/bxar/iblock/RepoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace marvin255\bxar\tests\iblock;
+
+use marvin255\bxar\tests\cases\Repo;
+
+class RepoTest extends Repo
+{
+	public function getObject()
+    {
+        return new \marvin255\bxar\iblock\Repo();
+    }
+}
