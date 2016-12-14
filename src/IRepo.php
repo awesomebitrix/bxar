91fd2f0dbabea149ddf5ab52ce4c6abe11990076
Виталий Акмашев <va@crtweb.ru>
2016-12-14 15:54:50 +0500

    add some methods for repo trait and tests for them

--- src/IRepo.php
+++ src/IRepo.php
@@ -7,4 +7,50 @@ namespace marvin255\bxar;
  */
 interface IRepo
 {
+    /**
+     * Возвращает объект-обработчик для поля.
+     *
+     * @param string $name
+     *
+     * @return \marvin255\bxar\IField
+     *
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    public function getFieldHandler($name);
+
+    /**
+     * Возвращает список с описаниями полей
+     *
+     * @return array
+     */
+    public function getFieldsDescriptions();
+
+    /**
+     * Задает класс моделей, которые будет создавать хранилище.
+     *
+     * @param string $modelClass
+     *
+     * @return \marvin255\bxar\IRepo
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function setModelClass($modelClass);
+
+    /**
+     * Возвращает класс моделей, которые будет создавать хранилище.
+     *
+     * @return string
+     */
+    public function getModelClass();
+
+    /**
+     * Обрабатывает название поля для того,
+     * чтобы привести все названия к единообразю.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    public function encodeFieldName($name);
 }
--- src/traits/Repo.php
+++ src/traits/Repo.php
@@ -3,6 +3,7 @@
 namespace marvin255\bxar\traits;
 
 use InvalidArgumentException;
+use LogicException;
 
 /**
  * Трэйт, который реализует базовые функции IRepo.
@@ -10,9 +11,83 @@ use InvalidArgumentException;
 trait Repo
 {
     /**
+     * @var array
+     */
+    protected $fieldsHandlers = [];
+
+    /**
+     * Возвращает объект-обработчик для поля.
+     *
+     * @param string $name
+     *
+     * @return \marvin255\bxar\IField
+     *
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    public function getFieldHandler($name)
+    {
+        $name = $this->encodeFieldName($name);
+        if (empty($this->fieldsHandlers[$name])) {
+            $descriptions = $this->getFieldsDescriptions();
+            if (!isset($descriptions[$name])) {
+                throw new InvalidArgumentException('Field not found: '.$name);
+            }
+            $field = $this->createFieldHandler($descriptions[$name]);
+            if (!($field instanceof \marvin255\bxar\IField)) {
+                throw new LogicException('Field must implements \marvin255\bxar\IField');
+            }
+            $this->fieldsHandlers[$name] = $field;
+        }
+        return $this->fieldsHandlers[$name];
+    }
+
+    /**
+     * Создает объект-обработчик для поля.
+     *
+     * @param array $description
+     *
+     * @return \marvin255\bxar\IField
+     */
+    protected function createFieldHandler(array $description)
+    {
+        return null;
+    }
+
+    /**
+     * @var array
+     */
+    protected $fieldsDescriptions = null;
+
+    /**
+     * Возвращает список с описаниями полей
+     *
+     * @return array
+     */
+    public function getFieldsDescriptions()
+    {
+        if ($this->fieldsDescriptions === null) {
+            $fields = $this->loadFieldsDescriptions();
+            $this->fieldsDescriptions = $fields ?: [];
+        }
+        return $this->fieldsDescriptions;
+    }
+
+    /**
+     * Возвращает список с описаниями полей из бд или иного источника,
+     * чья обработка занимает время
+     *
+     * @return array
+     */
+    protected function loadFieldsDescriptions()
+    {
+        return [];
+    }
+
+    /**
      * @var string
      */
-    protected $_modelClass = '';
+    protected $modelClass = '';
 
     /**
      * Задает класс моделей, которые будет создавать хранилище.
@@ -28,7 +103,7 @@ trait Repo
         if (!is_subclass_of($modelClass, '\\marvin255\bxar\\IModel')) {
             throw new InvalidArgumentException("Model class {$modelClass} must implements \marvin255\bxar\IModel");
         }
-        $this->_modelClass = $modelClass;
+        $this->modelClass = $modelClass;
         return $this;
     }
 
@@ -39,7 +114,7 @@ trait Repo
      */
     public function getModelClass()
     {
-        return $this->_modelClass;
+        return $this->modelClass;
     }
 
     /**
